29ede8b4661e870ee36477689b75fb1bd1856631
Alexander Carr <1alexander.carr@gmail.com>
2026-04-25 14:56:00 +0100

    custom tool now added to return current date and time

--- travel_agent/agent.py
+++ travel_agent/agent.py
@@ -1,10 +1,18 @@
 from google.adk.agents import Agent
+from datetime import datetime
+
+def now() -> dict:
+    """Returns the current date and time."""
+    my_datetime = datetime.now()
+    return {
+        "status": "success",
+        "current_time": str(my_datetime)
+    }
 
 root_agent = Agent(
-    name="root_agent",
+    name="travel_basic",
     model="gemini-2.5-flash",
-    instruction=(
-        "You are a helpful travel assistant. "
-        "You can help with general travel advice based on your knowledge."
-    ),
+    instruction="You are a helpful travel assistant."
+    + " You can help with general travel advice based on your knowledge.",
+    tools=[now]
 )
